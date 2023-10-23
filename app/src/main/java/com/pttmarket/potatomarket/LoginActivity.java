d19b428738db3889683987d54be697dfc04b47a3
kim yunsoo <pph41@naver.com>
2023-10-23 17:29:10 +0900

    INIT (LOGIN, REGISTRY : kys)

---
+++ app/src/main/java/com/pttmarket/potatomarket/LoginActivity.java
@@ -0,0 +1,72 @@
+package com.pttmarket.potatomarket;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef; //리얼타임 db
+    private EditText mEtEmail, mEtPwd;
+    private Button mBtnRegister, mBtnLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+        mBtnRegister = findViewById(R.id.btn_register);
+        mBtnLogin = findViewById(R.id.btn_login);
+
+        // 액티비티 실행부
+        mBtnLogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                String strEmail = mEtEmail.getText().toString();
+                String strPwd = mEtPwd.getText().toString();
+
+                mFirebaseAuth.signInWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //로그인 성공시
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            Toast.makeText(LoginActivity.this, "로그인 실패", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                }
+                );
+            }
+        });
+        //회원가입 화면 전환
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/pttmarket/potatomarket/RegisterActivity.java
@@ -0,0 +1,76 @@
+package com.pttmarket.potatomarket;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+    private DatabaseReference mDatabaseRef; //리얼타임 db
+    private EditText mEtEmail, mEtPwd;
+    private Button mBtnRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        mEtEmail = findViewById(R.id.et_email);
+        mEtPwd = findViewById(R.id.et_pwd);
+        mBtnRegister = findViewById(R.id.btn_register);
+
+        mBtnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                // 회원가입 처리
+                String strEmail = mEtEmail.getText().toString()+"@gwnu.ac.kr";
+                String strPwd = mEtPwd.getText().toString();
+
+                //파베 인증
+                mFirebaseAuth.createUserWithEmailAndPassword(strEmail, strPwd).addOnCompleteListener(RegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                   @Override
+                   public void onComplete(@NonNull Task<AuthResult> task){
+                       if (task.isSuccessful()) {
+                           FirebaseUser firebaseUser = mFirebaseAuth.getCurrentUser();
+                           UserAccount account = new UserAccount();
+                           account.setIdToken(firebaseUser.getUid());
+                           account.setEmailId(firebaseUser.getEmail());
+                           account.setPassword(strPwd);
+
+                           //db에 계정등록, Uid 링크
+                           mDatabaseRef.child("UserAccount").child(firebaseUser.getUid()).setValue(account);
+
+                           // 작동 시 메시지 출력
+                           Toast.makeText(RegisterActivity.this, "정상적으로 가입되었습니다.", Toast.LENGTH_SHORT).show();
+                           Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                           startActivity(intent);
+                       }else {
+                           Toast.makeText(RegisterActivity.this,"회원가입에 실패했습니다.", Toast.LENGTH_SHORT).show();
+                       }
+                   }
+
+                });
+
+
+            }
+        });
+    }
+}
