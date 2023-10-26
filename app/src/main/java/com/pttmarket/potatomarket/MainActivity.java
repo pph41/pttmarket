247e8c829e85d14630e09e9eb7ec21f4308f794b
KimGoEun <56550401+seru0021@users.noreply.github.com>
2023-10-26 17:22:40 +0900

    채팅, 채팅리스트, 네비게이션바, 화면전환 등 코드 추가.

--- app/src/main/java/com/pttmarket/potatomarket/MainActivity.java
+++ app/src/main/java/com/pttmarket/potatomarket/MainActivity.java
@@ -1,14 +1,40 @@
 package com.pttmarket.potatomarket;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_main);
+
+        // 채팅 버튼 클릭 시
+        Button chatButton = findViewById(R.id.chatButton);
+        chatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 채팅 버튼을 누를 때의 동작을 정의합니다.
+                // 현재 액티비티에서 채팅 목록 화면으로 이동
+                Intent intent = new Intent(MainActivity.this, ChatListActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        // 내 정보 버튼 클릭 시
+        Button myProfileButton = findViewById(R.id.myProfileButton);
+        myProfileButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 내 정보 버튼을 누를 때의 동작을 정의합니다.
+                // 현재 액티비티에서 사용자 프로필 화면으로 이동
+                Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
