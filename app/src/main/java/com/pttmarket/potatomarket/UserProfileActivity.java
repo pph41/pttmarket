5d78e848ca0c2805462e0a2ecdb5fbae9007b752
wonje <jjwwjj1215@naver.com>
2023-11-18 22:48:09 +0900

    게시판 id저장 추가 and 시간표 등록기능

--- app/src/main/java/com/pttmarket/potatomarket/UserProfileActivity.java
+++ app/src/main/java/com/pttmarket/potatomarket/UserProfileActivity.java
@@ -1,18 +1,77 @@
 package com.pttmarket.potatomarket;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
+import android.widget.Toast;
+
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class UserProfileActivity extends AppCompatActivity {
-
+    private Uri imageUri;
+    private ImageView imageView;
+    private FirebaseStorage storage;
+    private String email;
+    private  final int GALLERY_CODE = 10;
+    private final FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        storage = FirebaseStorage.getInstance();
+        imageView = findViewById(R.id.schedule);
+        Button upload_schedule = findViewById(R.id.myScheduleButton);
+        email = currentUser.getEmail();
+
+        FirebaseStorage storage_schedule = FirebaseStorage.getInstance();
+        StorageReference storageRef = storage_schedule.getReference();
+        storageRef.child(""+email).getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Glide.with(getApplicationContext())
+                        .load(uri)
+                        .into(imageView);
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(getApplicationContext(),"시간표를 등록해주세요!",Toast.LENGTH_SHORT).show();
+            }
+        });
+        upload_schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                startActivityForResult(intent,GALLERY_CODE);
+            }
+        });
+
 
         // 팔아요 버튼 클릭 시
         Button sellButton = findViewById(R.id.sellButton);
@@ -31,5 +90,16 @@ public class UserProfileActivity extends AppCompatActivity {
                 startActivity(new Intent(UserProfileActivity.this, ChatListActivity.class));
             }
         });
+
+    }
+    @Override
+    protected void onActivityResult(int requsetCode,final int resultCode,final Intent data) {
+        super.onActivityResult(requsetCode, resultCode, data);
+        if (requsetCode == GALLERY_CODE){
+            Uri file = data.getData();
+            StorageReference storageRef = storage.getReference();
+            StorageReference riversRef = storageRef.child(email);
+            UploadTask uploadTask = riversRef.putFile(file);
+        }
     }
 }
